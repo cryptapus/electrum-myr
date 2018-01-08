61e52a9b0852f920903494addb55e25e5333ab5c
cryptapus <info@cryptap.us>
2018-01-08 10:43:23 -0600

    Password-protect the JSON RPC interface
    From upstream and reworked for python2.7:
    https://github.com/spesmilo/electrum/commit/af527b21fb4e62df6f106ac4ad019e7708eb32ad

--- lib/daemon.py
+++ lib/daemon.py
@@ -29,12 +29,12 @@ import sys
 import time
 
 import jsonrpclib
-from jsonrpclib.SimpleJSONRPCServer import SimpleJSONRPCServer
+from .jsonrpc import VerifyingJSONRPCServer
 
 from version import ELECTRUM_VERSION
 from network import Network
 from util import json_decode, DaemonThread
-from util import print_msg, print_error, print_stderr
+from util import print_msg, print_error, print_stderr, to_string
 from wallet import WalletStorage, Wallet
 from commands import known_commands, Commands
 from simple_config import SimpleConfig
@@ -72,7 +72,14 @@ def get_server(config):
         try:
             with open(lockfile) as f:
                 (host, port), create_time = ast.literal_eval(f.read())
-                server = jsonrpclib.Server('http://%s:%d' % (host, port))
+                rpc_user, rpc_password = get_rpc_credentials(config)
+                if rpc_password == '':
+                    # authentication disabled
+                    server_url = 'http://%s:%d' % (host, port)
+                else:
+                    server_url = 'http://%s:%s@%s:%d' % (
+                        rpc_user, rpc_password, host, port)
+                server = jsonrpclib.Server(server_url)
             # Test daemon is running
             server.ping()
             return server
@@ -83,6 +90,32 @@ def get_server(config):
         # Sleep a bit and try again; it might have just been started
         time.sleep(1.0)
 
+
+def get_rpc_credentials(config):
+    rpc_user = config.get('rpcuser', None)
+    rpc_password = config.get('rpcpassword', None)
+    def to_bytes(n, length, endianess='big'):
+        """to_bytes for python 2.7"""
+        h = '%x' % n
+        s = ('0'*(len(h) % 2) + h).zfill(length*2).decode('hex')
+        return s if endianess == 'big' else s[::-1]
+    if rpc_user is None or rpc_password is None:
+        rpc_user = 'user'
+        import ecdsa, base64
+        bits = 128
+        nbytes = bits // 8 + (bits % 8 > 0)
+        pw_int = ecdsa.util.randrange(pow(2, bits))
+        #pw_b64 = base64.b64encode(
+        #    pw_int.to_bytes(nbytes, 'big'), b'-_')
+        pw_b64 = base64.b64encode(
+            to_bytes(pw_int, nbytes, 'big'), b'-_')
+        rpc_password = to_string(pw_b64, 'ascii')
+        config.set_key('rpcuser', rpc_user)
+        config.set_key('rpcpassword', rpc_password, save=True)
+    elif rpc_password == '':
+        from .util import print_stderr
+        print_stderr('WARNING: RPC authentication is disabled.')
+    return rpc_user, rpc_password
 
 
 class Daemon(DaemonThread):
@@ -110,8 +143,13 @@ class Daemon(DaemonThread):
     def init_server(self, config, fd):
         host = config.get('rpchost', '127.0.0.1')
         port = config.get('rpcport', 0)
+
+        rpc_user, rpc_password = get_rpc_credentials(config)
         try:
-            server = SimpleJSONRPCServer((host, port), logRequests=False)
+            #server = VerifyingJSONRPCServer((host, port), logRequests=False,
+            #                                rpc_user=rpc_user, rpc_password=rpc_password)
+            server = VerifyingJSONRPCServer((host, port),
+                                            rpc_user=rpc_user, rpc_password=rpc_password, logRequests=False)
         except:
             self.print_error('Warning: cannot initialize RPC server on host', host)
             self.server = None
